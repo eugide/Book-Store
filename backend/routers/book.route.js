7b288f796c12700133bb11141a059b8a98c49751
eugide <eugidendungutse770@gmail.com>
2025-01-31 09:27:19 +0200

    add get single book

--- backend/routers/book.route.js
+++ backend/routers/book.route.js
@@ -1,16 +1,18 @@
 import express from "express";
 
-import { creatBook, deleteBook, getBook, getBooks, uptudateBook } from "../controllers/book.controller.js";
+import {
+  creatBook,
+  deleteBook,
+  getBook,
+  getBooks,
+  uptudateBook,
+} from "../controllers/book.controller.js";
 
 const router = express.Router();
 
- 
-
-
-router.get("/", getBook);
-router.get("/:id", getBooks);
+router.get("/", getBooks);
+router.get("/:id", getBook);
 router.post("/", creatBook);
-
 router.put("/:id", uptudateBook);
 router.delete("/:id", deleteBook);
 

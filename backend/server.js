c0b077d0f27156875416e1668875370583220671
eugide <eugidendungutse770@gmail.com>
2025-02-05 10:24:46 +0200

    My changes

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,19 @@ import dotenv from "dotenv";
 import { connectDb } from "./config/db.js";
 import bookRouter from "./routers/book.route.js";
 
+
+
+
 dotenv.config();
 
 const app = express();
+
+const port = process.env.PORT;
+
 app.use(express.json());
 app.use("/api/books", bookRouter);
 
-app.listen(4000, async () => {
+app.listen(port, async () => {
   await connectDb();
-  console.log("Server started on port 4000");
+  console.log(`Server started on port ${port}`);
 });
